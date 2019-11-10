1dccf464b8def09e1b7b41c62e55e37ed5da2ef6
@angelwarrior76 <aleksandr.jusupov@gmail.com>
2019-11-10 13:27:23 +0200

    добавил переопределение equals и hashCode

--- src/main/java/com/homework/spring/service/test/model/Currency.java
+++ src/main/java/com/homework/spring/service/test/model/Currency.java
@@ -1,5 +1,7 @@
 package com.homework.spring.service.test.model;
 
+import com.homework.spring.service.test.repositories.HibernateSessionFactory;
+
 import java.io.Serializable;
 
 import javax.persistence.Column;
@@ -26,9 +28,11 @@ public class Currency implements Serializable {
     private String description;
 
     protected Currency() {
+        super();
     }
 
     public Currency(int code, String mnemonics, String description) {
+        super();
         this.code = code;
         this.mnemonics = mnemonics;
         this.description = description;
@@ -61,6 +65,40 @@ public class Currency implements Serializable {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.code;
+        hash = 53 * hash + (this.mnemonics != null ? this.mnemonics.hashCode() : 0);
+        hash = 53 * hash + (this.description != null ? this.description.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Currency other = (Currency) obj;
+        if (this.code != other.code) {
+            return false;
+        }
+        if (this.mnemonics != other.mnemonics && (this.mnemonics == null || !this.mnemonics.equals(other.mnemonics))) {
+            return false;
+        }
+        if (this.description != other.description && (this.description == null || !this.description.equals(other.description))) {
+            return false;
+        }
+        return true;
+    }
+
+    public static Currency valueOf(int code) {
+        return HibernateSessionFactory.getSessionFactory().openSession().get(Currency.class, code);
     }
 
 }
