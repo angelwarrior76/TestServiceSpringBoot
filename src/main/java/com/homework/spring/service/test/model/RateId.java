d0b3520f0008524caa9e6f83f7e98b5239ae9db6
@angelwarrior76 <aleksandr.jusupov@gmail.com>
2019-11-10 13:28:48 +0200

    добавил переопределение equals и hashCode

--- src/main/java/com/homework/spring/service/test/model/RateId.java
+++ src/main/java/com/homework/spring/service/test/model/RateId.java
@@ -6,13 +6,42 @@ public class RateId implements Serializable {
 
     private static final long serialVersionUID = 8301270921007651560L;
     private int cdate;
-    private int code;
+    private Currency currency;
 
-    public RateId() {}
+    public RateId() {
+        super();
+    }
 
     public RateId(int cdate, int code) {
+        super();
         this.cdate = cdate;
-        this.code = code;
+        this.currency = Currency.valueOf(code);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + this.cdate;
+        hash = 67 * hash + (this.currency != null ? this.currency.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RateId other = (RateId) obj;
+        if (this.cdate != other.cdate) {
+            return false;
+        }
+        if (this.currency != other.currency && (this.currency == null || !this.currency.equals(other.currency))) {
+            return false;
+        }
+        return true;
     }
 
 }
