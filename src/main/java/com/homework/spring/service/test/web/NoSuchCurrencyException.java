23533b69d3d624262aafd76e88047e7e851ba444
@angelwarrior76 <aleksandr.jusupov@gmail.com>
2019-11-10 13:32:02 +0200

    добавил определение кода ошибки

--- src/main/java/com/homework/spring/service/test/web/NoSuchCurrencyException.java
+++ src/main/java/com/homework/spring/service/test/web/NoSuchCurrencyException.java
@@ -2,12 +2,24 @@ package com.homework.spring.service.test.web;
 
 public class NoSuchCurrencyException extends Exception {
 
+    private String code;
+
     NoSuchCurrencyException() {
         super();
+        this.code = null;
     }
 
     NoSuchCurrencyException(String message) {
         super(message);
+        this.code = null;
     }
 
+    NoSuchCurrencyException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
